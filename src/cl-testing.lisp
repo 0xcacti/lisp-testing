5b8192a028b8672a03d578d6651415e72e2dea23
0xcacti <0xcacti@gmail.com>
2024-11-19 13:40:31 -0500

    I don't like that you can put anything you want in a group, but it's fine

--- src/cl-testing.lisp
+++ src/cl-testing.lisp
@@ -3,9 +3,11 @@
 (defmacro describe-group (group-name &body body)
   (let ((group-name-gensym (gensym)))
     `(let ((,group-name-gensym ,group-name))
+       (format t "~&~A:~%" ,group-name-gensym)
+       (loop for i from 0 to (length (string ,group-name-gensym)) do
+             (format t "="))
+       (format t "~%")
        ,@body)))
 
-
-
-
-
+(describe-group "A group of mathematics tests"
+    (+ 3 3))
