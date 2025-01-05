538b3a16e4d101a2694fc5b43f18f311f7402f43
0xcacti <0xcacti@gmail.com>
2025-01-05 00:05:19 -0500

    add the ability to do ansi coloration

--- src/cl-testing.lisp
+++ src/cl-testing.lisp
@@ -1,5 +1,22 @@
 (in-package #:cl-testing)
 
+(defparameter *colors*
+  `(:black   ,(format nil "~C[30m" #\Escape)
+    :red     ,(format nil "~C[31m" #\Escape)
+    :green   ,(format nil "~C[32m" #\Escape)
+    :yellow  ,(format nil "~C[33m" #\Escape)
+    :blue    ,(format nil "~C[34m" #\Escape)
+    :magenta ,(format nil "~C[35m" #\Escape)
+    :cyan    ,(format nil "~C[36m" #\Escape)
+    :white   ,(format nil "~C[37m" #\Escape)
+    :reset   ,(format nil "~C[0m" #\Escape)))
+
+(defmacro cformat (color &rest body)
+  `(progn
+    (format t "~a" (getf *colors* ,color))
+    ,@body
+    (format t "~a" (getf *colors* :reset))))
+
 (defmacro describe-group (group-name &body body)
   (let ((group-name-gensym (gensym)))
     `(let ((,group-name-gensym ,group-name))
@@ -9,5 +26,21 @@
        (format t "~%")
        ,@body)))
 
-(describe-group "A group of mathematics tests"
-    (+ 3 3))
+(defmacro it (test-name &body body)
+  (let ((test-name-gensym (gensym)))
+    `(let ((,test-name-gensym ,test-name))
+       (format t "~&~A |> " ,test-name-gensym)
+       (handler-case 
+           (progn
+             ,@body 
+             (cformat :green (format t "PASSED~%")))
+         (error (e)
+           (cformat :red (format t "FAILED: ~A~%" e)))))))
+
+(describe-group "A group of mathematics tests" 
+  (it "should add two numbers" 
+    (assert (= (+ 3 4) 6)))
+
+  (it "should subtract two numbers"
+    (assert (= (- 3 4) -1)))
+  )
